987ab634a2df8ecef3cf643ebfd49c414c1a3747
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-22 20:29:12 +0900

    created the view for the home page

--- CMSC495/bug_tracker/views.py
+++ CMSC495/bug_tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    # boiler plate code until template is created
+    return HttpResponse("CMSC")
