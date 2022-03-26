40e71e84c1c9548714e1cda0327c6b8cfe04d222
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-26 09:45:03 +0900

    corrected import

--- CMSC495/bug_tracker/admin.py
+++ CMSC495/bug_tracker/admin.py
@@ -1,5 +1,5 @@
 """ Module is used to register all models with the Django Admin Site"""
 from django.contrib import admin
-from models import BugTracker
+from bug_tracker.models import BugTracker
 
 admin.site.register(BugTracker)
