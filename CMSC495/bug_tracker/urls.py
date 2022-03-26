befaad6ac4d347414b7697cf44b442c20831cf8a
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-26 14:08:39 +0900

    Created the path for the detail view

--- CMSC495/bug_tracker/urls.py
+++ CMSC495/bug_tracker/urls.py
@@ -1,9 +1,11 @@
+from unicodedata import name
 from django.urls import path
 
-from bug_tracker.views import IndexListView, BugCreateView
+from bug_tracker.views import IndexListView, BugCreateView, BugDetailView
 
 urlpatterns = [
     # Home page
     path('', IndexListView.as_view(), name='index_list'),
-    path('create/', BugCreateView.as_view(), name='create_bug')
+    path('create/', BugCreateView.as_view(), name='create_bug'),
+    path('<int:pk>/',  BugDetailView.as_view(), name='bug_detail')
 ]
