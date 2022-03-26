7e9e0b7ccd3656f6a074f1513f6a9365beb8c18d
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-26 13:42:39 +0900

    create view created

--- CMSC495/bug_tracker/views.py
+++ CMSC495/bug_tracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.http import HttpResponse
 from bug_tracker.models import BugTracker
 
@@ -7,7 +7,14 @@ from bug_tracker.models import BugTracker
 class IndexListView(ListView):
     """ View will display all Bug Tracker objects in a list on the hompage
 
-    Args:
-        ListView (_type_): View that displays all objects as list at the users discretion
     """
     model = BugTracker
+
+
+class BugCreateView(CreateView):
+    """View will give user ability to create new bug object
+
+    """
+    model = BugTracker
+    fields = ["bug_title", 'project_name', 'date_occured',
+              'bug_description', 'bug_risk']
