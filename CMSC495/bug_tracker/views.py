59928915e7190b139767b0eaabf414f0c9293ecf
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-26 10:22:09 +0900

    created list view for home page

--- CMSC495/bug_tracker/views.py
+++ CMSC495/bug_tracker/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
+from bug_tracker.models import BugTracker
 
 
-# Create your views here.
-def index(request):
-    # boiler plate code until template is created
-    return HttpResponse("CMSC")
+class IndexListView(ListView):
+    """ View will display all Bug Tracker objects in a list on the hompage
+
+    Args:
+        ListView (_type_): View that displays all objects as list at the users discretion
+    """
+    model = BugTracker
