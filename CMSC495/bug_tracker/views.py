c886d232d2590336c44b0fbe2e1994051beb2de3
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-26 17:57:47 +0900

    documentation

--- CMSC495/bug_tracker/views.py
+++ CMSC495/bug_tracker/views.py
@@ -6,28 +6,29 @@ from bug_tracker.models import BugTracker
 
 
 class IndexListView(ListView):
-    """ View will display all Bug Tracker objects in a list on the hompage
+    """ View will display all Bug Tracker objects in a list on the hompage. """
 
-    """
     model = BugTracker
 
 
 class BugCreateView(CreateView):
-    """View will give user ability to create new bug object
+    """ View will give user ability to create new Bug Tracker objects. """
 
-    """
     model = BugTracker
     fields = ["bug_title", 'project_name', 'date_occured',
               'bug_description', 'bug_risk']
 
 
 class BugDetailView(DetailView):
+    """ View will display the Bug Tracker object in its entirety. """
 
     model = BugTracker
     context_object_name = 'bug'
 
 
 class BugUpdateView(UpdateView):
+    """View will display a form so the user can update the bug tracker object. """
+
     model = BugTracker
     fields = ["bug_title", 'project_name', 'date_occured',
               'bug_description', 'bug_risk']
