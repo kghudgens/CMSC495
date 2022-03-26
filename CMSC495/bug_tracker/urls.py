cca90c47076e4f8d5eaf8f70b8becd8a21acc5d5
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-26 13:42:14 +0900

    created url path for the create view

--- CMSC495/bug_tracker/urls.py
+++ CMSC495/bug_tracker/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from bug_tracker.views import IndexListView
+from bug_tracker.views import IndexListView, BugCreateView
 
 urlpatterns = [
     # Home page
     path('', IndexListView.as_view(), name='index_list'),
+    path('create/', BugCreateView.as_view(), name='create_bug')
 ]
