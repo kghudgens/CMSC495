dac767770edd47fc65dddbb1cee5760c58fc597f
Kevin Hudgens <hudgens1073@gmail.com>
2022-03-29 16:48:01 +0900

    added the url path for the delete view

--- CMSC495/bug_tracker/urls.py
+++ CMSC495/bug_tracker/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from bug_tracker.views import IndexListView, BugCreateView, BugDetailView, BugUpdateView
+from bug_tracker.views import IndexListView, BugCreateView, BugDetailView, BugUpdateView, BugDeleteView
 
 urlpatterns = [
     # Home page
     path('', IndexListView.as_view(), name='index_list'),
     path('create/', BugCreateView.as_view(), name='create_bug'),
     path('<int:pk>/',  BugDetailView.as_view(), name='bug_detail'),
-    path('<int:pk>/update/', BugUpdateView.as_view(), name='bug_update')
+    path('<int:pk>/update/', BugUpdateView.as_view(), name='bug_update'),
+    path('<int:pk>/delete', BugDeleteView.as_view(), name='bug_delete')
 ]
