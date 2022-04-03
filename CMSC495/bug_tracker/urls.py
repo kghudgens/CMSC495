d6330119e0bd0177315faa060b392445ce359109
Kevin Hudgens <hudgens1073@gmail.com>
2022-04-03 17:39:09 +0900

    created path to both home and search pages

--- CMSC495/bug_tracker/urls.py
+++ CMSC495/bug_tracker/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from bug_tracker.views import IndexListView, BugCreateView, BugDetailView, BugUpdateView, BugDeleteView, about
+from bug_tracker.views import IndexListView, BugCreateView, BugDetailView, BugUpdateView, BugDeleteView, aboutView, BugSearchListView, homeView
 
 urlpatterns = [
-    # Home page
-    path('', IndexListView.as_view(), name='index_list'),
-    path('about/', about, name='about_page'),
+    path('', homeView, name="home"),
+    path('bugs', IndexListView.as_view(), name='bug_list'),
+    path('about/', aboutView, name='about_page'),
     path('create/', BugCreateView.as_view(), name='create_bug'),
     path('<int:pk>/',  BugDetailView.as_view(), name='bug_detail'),
     path('<int:pk>/update/', BugUpdateView.as_view(), name='bug_update'),
-    path('<int:pk>/delete', BugDeleteView.as_view(), name='bug_delete')
+    path('<int:pk>/delete', BugDeleteView.as_view(), name='bug_delete'),
+    path('search/', BugSearchListView.as_view(), name="search_results")
 ]
